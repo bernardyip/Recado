26486ee551677d82ec7f676ca2e70eba0f501d36
bernardyip <bernard_yip40@hotmail.com>
2017-02-19 00:32:19 +0800

    Add count of task in index.php

--- index.php
+++ index.php
@@ -10,12 +10,19 @@
 		$dbcon = pg_connect('host=localhost dbname=postgres user=postgres password=password');
 		pg_prepare($dbcon, 'select_category_query', "SELECT c.id, c.name FROM public.category c");
 		$result = pg_execute($dbcon, 'select_category_query', array());
-		while ($row = pg_fetch_array($result)) { ?>
-			<h3><?=$row['name']?></h3>
+		while ($row = pg_fetch_array($result)) { 
+			pg_prepare($dbcon, 'count_category_query', "SELECT COUNT(*) as count FROM public.task t WHERE t.category_id=$1");
+			$count_result = pg_execute($dbcon, 'count_category_query', array($row['id']));
+			$count_result = pg_fetch_array($count_result); ?>
+			<h3><?=$row['name']?> (<?=$count_result['count']?> tasks)</h3>
 	<?php 	pg_prepare($dbcon, 'select_task_in_category_query', "SELECT t.id, t.name, t.description FROM public.task t WHERE t.category_id=$1 LIMIT 3");
 			$cat_result = pg_execute($dbcon, 'select_task_in_category_query', array($row['id']));
 			while ($task_row = pg_fetch_array($cat_result)) {
 				echo $task_row['name'] . "<br />";
+			}
+			
+			if ($count_result['count'] > 3) {
+				echo "more...<br />";
 			}
 		}
 		?>
