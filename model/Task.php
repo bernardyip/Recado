0c6ca021b3cf2a5019d726b0e10bc0252ad1ec9e
qhng <xefrog@live.cn>
2017-03-26 00:15:57 +0800

    Fix bug in Task entity model. Check for instance of DateTime before creating an object.

--- model/Task.php
+++ model/Task.php
@@ -26,11 +26,32 @@ class Task {
         $this->description = trim($description);
         $this->postalCode = (int)$postalCode;
         $this->location = trim($location);
-        $this->taskStartTime = new DateTime($taskStartTime, new DateTimeZone('Asia/Singapore'));
-        $this->taskEndTime = new DateTime($taskEndTime, new DateTimeZone('Asia/Singapore'));;
+        
+        if ($taskStartTime instanceof DateTime) {
+            $this->taskStartTime = $taskStartTime;
+        } else {
+            $this->taskStartTime = new DateTime($taskStartTime, new DateTimeZone('Asia/Singapore'));
+        }
+        
+        if ($taskEndTime instanceof DateTime) {
+            $this->taskEndTime = $taskEndTime;
+        } else {
+            $this->taskEndTime = new DateTime($taskEndTime, new DateTimeZone('Asia/Singapore'));
+        }
+        
         $this->listingPrice = ConversionHelper::stringToMoney($listingPrice);
-        $this->createdTime = new DateTime($createdTime, new DateTimeZone('Asia/Singapore'));
-        $this->updatedTime = new DateTime($updatedTime, new DateTimeZone('Asia/Singapore'));
+        
+        if ($createdTime instanceof DateTime) {
+            $this->createdTime = $createdTime;
+        } else {
+            $this->createdTime = new DateTime($createdTime, new DateTimeZone('Asia/Singapore'));
+        }
+        
+        if ($updatedTime instanceof DateTime) {
+            $this->updatedTime = $updatedTime;
+        } else {
+            $this->updatedTime = new DateTime($updatedTime, new DateTimeZone('Asia/Singapore'));
+        }
         $this->status = trim($status);
         $this->bidPicked = $bidPicked === "t";
         $this->categoryId = (int)$categoryId;
